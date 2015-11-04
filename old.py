9d92c9f6928601e96163dd6d3d58f3409db4f1d7
Diogo Ferreira <Diogo Ferreira>
2015-11-04 15:45:16 +0000

    Adrian is nub

---
+++ old.py
@@ -0,0 +1,15 @@
+#! /usr/local/bin/python3
+# Adrian is <insert insult here>
+import sys
+
+out_of = int(sys.argv[1])
+while 1:
+    try:
+        line = input()
+    except Exception:
+        continue
+    total = 0
+    for num in line.split(" "):
+        total += int(num)
+    print("Total: %d (%f%%)" % (total, total * (100.0 / out_of)))
+print("exit")
